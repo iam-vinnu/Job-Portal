156a42bd4547faa141a5b1b0301b6400b3663789
iam-vinnu <binaybehera101@gmail.com>
2024-08-14 21:07:35 +0530

    basic backend code

---
+++ Backend/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+const corsOptions = {
+        origin:'http://localhost:5173',
+        Credential:true
+}
+app.use(cors(corsOptions));
+const PORT = 3000;
+app.listen(PORT,()=>{
+    console.log(`server running at port ${PORT}`);
+})
+
+
