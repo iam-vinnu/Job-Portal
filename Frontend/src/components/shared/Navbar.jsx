68e45963bf01886b73dddafb9dd8ba814aff09db
iam-vinnu <binaybehera101@gmail.com>
2024-08-29 14:03:06 +0530

    nothing

--- Frontend/src/components/shared/Navbar.jsx
+++ Frontend/src/components/shared/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
                 <div>
                     <h1 className='text-2xl font-bold'>Job<span className='text-[#f83002]'>Portal</span></h1>
                 </div>
-                <div className='flex items-center gap-12'>
+                <div className='hidden md:flex items-center gap-12'>
                     <ul className='flex font-medium items-center gap-5'>
                         <li>Home</li>
                         <li>Jobs</li>
