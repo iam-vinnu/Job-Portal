6a912b0efa64ebbbad2cb94d5318dbc0509104dc
iam-vinnu <binaybehera101@gmail.com>
2024-09-06 20:58:35 +0530

    signup page

--- Frontend/src/components/auth/Signup.jsx
+++ Frontend/src/components/auth/Signup.jsx
@@ -3,11 +3,13 @@ import Navbar from '../shared/Navbar'
 import { Label } from '../ui/label'
 import { Input } from '../ui/input'
 import { RadioGroup, RadioGroupItem } from '../ui/radio-group'
+import { Button } from '../ui/button'
+import { Link } from 'react-router-dom'
 
 export const Signup = () => {
   return (
     <div>
-      <Navbar />
+      <Navbar/>
 
       <div className='flex items-center justify-center max-w-6xl mx-auto'>
         <form action="" className='w-1/2 border border-gray-200 rounded-md p-4 my-10 shadow-lg'>
@@ -28,19 +30,39 @@ export const Signup = () => {
             <Label>Password</Label>
             <Input type="password" placeholder='' />
           </div>
-          <div className='flex justify-center items-center'>
-            <RadioGroup defaultValue="option-one" className="flex items-center gap-4 my-5">
+          <div className='flex justify-between items-center'>
+            <RadioGroup className="flex items-center gap-4 my-5">
               <div className="flex items-center space-x-2">
-                <RadioGroupItem value="option-one" id="option-one" />
-                <Label htmlFor="option-one">Recruiter</Label>
+                <Input
+                  type="radio"
+                  name="role"
+                  value='student'
+                  className='cursor-pointer'
+                />
+                <Label htmlFor="option-one">Student</Label>
               </div>
               <div className="flex items-center space-x-2">
-                <RadioGroupItem value="option-two" id="option-two" />
-                <Label htmlFor="option-two">Student</Label>
+                <Input
+                  type="radio"
+                  name="role"
+                  value='recruiter'
+                  className='cursor-pointer'
+                />
+                <Label htmlFor="option-two">Recruiter</Label>
               </div>
             </RadioGroup>
+            <div className='flex items-center gap-2'>
+              <Label>Profile</Label>
+              <Input
+                accept='image/*'
+                type='file'
+                className='cursor-pointer'
+              />
 
+            </div>
           </div>
+          <Button type='submit' className='bg-[#2e52e6] hover:bg-[#1f3cb0] w-full mb-3'>Signup</Button>
+          <span className=''>Already have an account? <Link to='/login'>Login</Link></span>
         </form>
       </div>
     </div>
