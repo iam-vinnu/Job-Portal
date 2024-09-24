b198d2b44fce1bd4a48c8546665cd69a280b7d62
iam-vinnu <binaybehera101@gmail.com>
2024-09-24 22:23:24 +0530

    init

---
+++ Frontend/src/components/LatestJobCards.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Badge } from './ui/badge'
+
+const LatestJobCards = () => {
+  return (
+    <div>
+      <div>
+        <h1>Comapny Name</h1>
+        <p>India</p>
+      </div>
+      <div>
+        <h1>Job Title</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+      </div>
+      <div>
+        <Badge>Positions</Badge>
+      </div>
+    </div>
+  )
+}
+
+export default LatestJobCards
