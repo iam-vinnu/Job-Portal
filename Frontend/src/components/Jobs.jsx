b35268b5273b1bf5d524d6ead2e5f6900e551adb
iam-vinnu <binaybehera101@gmail.com>
2024-10-05 23:23:24 +0530

    updated

--- Frontend/src/components/Jobs.jsx
+++ Frontend/src/components/Jobs.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './shared/Navbar'
 import FilterCard from './FilterCard'
 import Job from './job'
+import { SpaceIcon } from 'lucide-react'
 
 const jobsArray = [1, 2, 3, 4, 5, 6, 7, 8]
 
@@ -12,12 +13,22 @@ function Jobs() {
 
       <div className='max-w-6xl mx-auto mt-5'>
         <div className='flex gap-5'>
-
-        <FilterCard />
-
-        {
-          jobsArray.map((item, index) => <Job />)
-        }
+          <div className='w-20%'>
+            <FilterCard />
+          </div>
+          {
+            jobsArray.length <= 0 ? <span>No Job Found</span> : (
+              <div className='flex-1 h-[88vh] overflow-y-auto pb-5'>
+                <div className='grid grid-cols-3 gap-5'>
+                  {jobsArray.map((item, index) => (
+                    <div>
+                      <Job />
+                    </div>
+                  ))}
+                </div>
+              </div>
+            )
+          }
         </div>
       </div>
 
