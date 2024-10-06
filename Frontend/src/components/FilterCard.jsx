06bc8a78b70f9a0ae354da364afcbbfa03b3c2d9
iam-vinnu <binaybehera101@gmail.com>
2024-10-06 22:37:46 +0530

    Job Page

--- Frontend/src/components/FilterCard.jsx
+++ Frontend/src/components/FilterCard.jsx
@@ -1,8 +1,52 @@
 import React from 'react'
+import { RadioGroup, RadioGroupItem } from './ui/radio-group'
+import { AreaChart } from 'lucide-react'
+import { Label } from './ui/label'
+
+const fillterData = [
+  {
+    fillerType:'Location',
+    array:['Delhi NCR' , 'Bangalore' , 'Hyderabad' , 'Pune' , 'Mumbai']
+  },
+  {
+    fillerType:'Industry',
+    array:['Frontend Developer',
+        'Backend Developer',
+        'FullStack Developer']
+  },
+  {
+    fillerType:'Salary',
+    array:['0-40k' , '42-1Lakh' , '1-5Lakh']
+  },
+]
+
+
 
 function FilterCard() {
   return (
-    <div>FilterCard</div>
+    <div className='w-full bg-white p-3 rounded-md'>
+       <h1 className='font-bold text-lg'>Fillter Jobs</h1>
+       <hr className='mt-3' />
+       <RadioGroup>
+        {
+          fillterData.map((item,index)=>(
+            <div>
+              <h1 className='font-bold text-lg'>{item.fillerType}</h1>
+              {
+                item.array.map((arrayItem , index)=>{
+                  return (
+                    <div className='flex items-center space-x-2 my-2'>
+                      <RadioGroupItem value={arrayItem}/>
+                      <Label>{arrayItem}</Label>
+                    </div>
+                  )
+                })
+              }
+            </div>
+          ))
+        }
+       </RadioGroup>
+    </div>
   )
 }
 
